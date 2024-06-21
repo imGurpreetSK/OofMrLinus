6c734e5446e356769e78436fe7cb332a5ad37edd
Gurpreet Singh <gurpreetsk@hotmail.com>
2024-06-21 15:50:07 -0700

    Implement RantsFileReader.

--- shared/src/commonMain/kotlin/com/gurpreetsk/oofmrlinus/repository/internal/FileReader.kt
+++ shared/src/commonMain/kotlin/com/gurpreetsk/oofmrlinus/repository/internal/FileReader.kt
@@ -1,7 +1,25 @@
 package com.gurpreetsk.oofmrlinus.repository.internal
 
+import com.gurpreetsk.oofmrlinus.repository.model.Rants
+import kotlinx.serialization.json.Json
+import oofmrlinus.shared.generated.resources.Res
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+
 typealias Line = String
 
 internal fun interface FileReader {
     suspend fun read(): List<Line>
+}
+
+internal class RantsFileReader(
+    private val json: Json
+) : FileReader {
+
+    @OptIn(ExperimentalResourceApi::class) // For Res.readBytes(...) usage.
+    override suspend fun read(): List<Line> = try {
+        val fileAsString = Res.readBytes("files/oofs.json").decodeToString()
+        json.decodeFromString<Rants>(fileAsString).rants
+    } catch (e: Exception) {
+        emptyList()
+    }
 }
